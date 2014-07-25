0e83b811185c3e4d5fa52251b77f7b7a3c50708c
Mateusz J <matektrooper@gmail.com>
2014-07-25 15:40:19 +0200

    Programming assignment done.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an implementation of special matrix that cache's its inverse.
 
-## Write a short comment describing this function
-
+## This function creates the special matrix object.
+## Arguments: x (optional) - the "normal" matrix you want to set to the
+## "special" matrix object.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        #This is for setting desired "normal" matrix to "special" matrix object.
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        #This is for getting the actual matrix from the special matrix object.
+        get <- function() x
+        
+        #This is for setting the matrix's inverse.
+        setInverse <- function(inverse) inv <<- inverse
+        
+        #This is for getting the matrix's inverse.
+        getInverse <- function() inv
+        
+        #Return the special matrix's operations list.
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This functions returns either cached or on-line computed matrix inverse
+## that the "special matrix" object holds.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        
+        if(!is.null(inv)) {
+                message("Getting cached matrix inverse.")
+                return(inv)
+        }
+        
+        mat <- x$get()
+        
+        inv <- solve(mat, ...)
+        
+        x$setInverse(inv)
+        
+        inv
 }
